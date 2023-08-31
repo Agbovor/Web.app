36ef9691bfc4c52c8e5e37852d3bd372ea091b71
agbovor <agbovoralex65@gmail.com>
2023-08-31 12:59:42 +0100

    Just App

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+from flask_mail import Mail, Message
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = str(os.getenv('MAIL_SERVER'))
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = str(os.getenv('MAIL_USERNAME'))
+app.config['MAIL_PASSWORD'] = str(os.getenv('MAIL_PASSWORD'))
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+@app.route('/mail')
+def send():
+    message = Message(
+        subject= "Button Event",
+        recipients = ["trumpalex108@gmail.com"],
+        sender = "agbovoralex65@gmail.com"
+
+    )
+
+    message.body = "I pushed the button"
+
+    try:
+        mail.send(message)
+        return "sent successfully"
+    
+    except Exception as e:
+        return "sent failed"
+
+# @app.route("/mail")
+# def whereami():
+#     return "<p>where are you indeed</p>"
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
